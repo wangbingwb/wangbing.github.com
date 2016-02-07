a57b92c7ce41b8119116004cc973a331b6ffa4c9
wangbing <wangbingit@outlook.com>
2016-02-07 22:27:52 +0800

    init

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 //日志目录api接口
 var rootUrl = "http://api.github.com/repos/wangbingwb/wb/contents/blog"
 
-var BodyCtrl = function ($scope,ContentService) {
+var BodyCtrl = function ($scope,ContentService,$sce) {
     $scope.vm = {
         isDoFind:true,
         isFinish:false,
@@ -42,7 +42,7 @@ var BodyCtrl = function ($scope,ContentService) {
         $scope.vm.isDoFind = false;
         $scope.vm.isFinish = false;
         var obj = $scope.result[index]
-        var url ="blog/"+obj.type+"/"+obj.time+"-"+obj.title
+        var url ="blog/"+obj.type+"/"+obj.time+"-"+obj.title+".md"
         ContentService.getContent(url).success(function (data) {
             $scope.article = {
                 title:$scope.result[index].title,
@@ -50,6 +50,9 @@ var BodyCtrl = function ($scope,ContentService) {
                 type:$scope.result[index].type,
                 content:data
             }
+            //$scope.article.content = $sce.trustAsHtml(markdown.toHTML(data));
+            //console.log(data)
+            setTimeout("toHtmlView()",10)
             $scope.vm.isFinish = true;
         });
     }
@@ -84,10 +87,11 @@ var BodyCtrl = function ($scope,ContentService) {
         $scope.pages.push({action:count,name:">>",isSelect:false});
         console.log($scope.pages)
     };
+
 }
 
 angular.module("index", [])
-    .controller("BodyCtrl", ["$scope","ContentService",BodyCtrl])
+    .controller("BodyCtrl", ["$scope","ContentService","$sce",BodyCtrl])
     .factory("ContentService", ['$http', function ($http) {
         var service = {};
         service.getList = function (data) {
@@ -105,3 +109,26 @@ angular.module("index", [])
         };
         return service;
     }])
+
+
+function toHtmlView(){
+    var target = $("#articleBody").val();
+
+    var testEditormdView = editormd.markdownToHTML("articleBody-view", {
+        //markdown        : data ,//+ "\r\n" + $("#append-test").text(),
+        markdown        : target ,//+ "\r\n" + $("#append-test").text(),
+        //htmlDecode      : true,       // 开启 HTML 标签解析，为了安全性，默认不开启
+        htmlDecode      : "style,script,iframe",  // you can filter tags decode
+        //toc             : false,
+        tocm            : true,    // Using [TOCM]
+        //tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
+        //gfm             : false,
+        //tocDropdown     : true,
+        // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
+        emoji           : true,
+        taskList        : true,
+        tex             : true,  // 默认不解析
+        flowChart       : true,  // 默认不解析
+        sequenceDiagram : true,  // 默认不解析
+    });
+}
